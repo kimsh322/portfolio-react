f82498dd45a9161e09ff5dab490ce585ef209f9a
kimsh322 <kimsh91322@gmail.com>
2023-09-18 22:08:59 +0900

    Refactor : Carousel 컴포넌트에서 CarouselContents 부분 새로운 컴포넌트로 작성

--- src/components/projects/carousel/Carousel.tsx
+++ src/components/projects/carousel/Carousel.tsx
@@ -3,13 +3,10 @@ import { BiLeftArrow, BiRightArrow } from "react-icons/bi";
 import { prevHandler, nextHandler, CarouselHandlerArgsType } from "./carouselHandler";
 import { useState } from "react";
 import { ProjectContent } from "../project-contents/typingPractice";
+import CarouselContents from "./CarouselContents";
 
 interface Props {
   project: ProjectContent;
-}
-interface CarouselBoxProps {
-  imageNumber: number;
-  carouselTransition: string;
 }
 
 const Carousel = ({ project }: Props) => {
@@ -35,15 +32,11 @@ const Carousel = ({ project }: Props) => {
       <button type="button" className="icon left">
         <BiLeftArrow onClick={() => prevHandler(carouselArgsArr)} />
       </button>
-      <CarouselBox imageNumber={imageNumber} carouselTransition={carouselTransition}>
-        {projectImages.map((image, idx) => {
-          return (
-            <div className="image-box">
-              <img src={image} key={idx} alt="project-image" className="project-image" />
-            </div>
-          );
-        })}
-      </CarouselBox>
+      <CarouselContents
+        imageNumber={imageNumber}
+        carouselTransition={carouselTransition}
+        projectImages={projectImages}
+      />
       <button type="button" className="icon right">
         <BiRightArrow onClick={() => nextHandler(carouselArgsArr)} />
       </button>
@@ -79,32 +72,6 @@ const CarouselContainer = styled.div`
     right: 1%;
     top: 50%;
     transform: translateY(-50%);
-  }
-`;
-
-const CarouselBox = styled.div<CarouselBoxProps>`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 95%;
-  padding: 0 20%;
-  background-color: gray;
-  transform: translateX(
-    ${({ imageNumber }) => {
-      return `${100 - imageNumber * 50}vw`;
-    }}
-  ); //720px씩 넘기기
-  transition: ${({ carouselTransition }) => carouselTransition};
-  .image-box {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 50vw;
-    height: 95%;
-    .project-image {
-      min-width: 80%;
-      max-height: 100%;
-    }
   }
 `;
 
---
+++ src/components/projects/carousel/CarouselContents.tsx
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+
+interface Props {
+  imageNumber: number;
+  carouselTransition: string;
+  projectImages: string[];
+}
+
+interface CarouselBoxProps {
+  imageNumber: number;
+  carouselTransition: string;
+}
+
+const CarouselContents = ({ imageNumber, carouselTransition, projectImages }: Props) => {
+  return (
+    <>
+      <CarouselContentsContainer imageNumber={imageNumber} carouselTransition={carouselTransition}>
+        {projectImages.map((image, idx) => {
+          return (
+            <div className="image-box">
+              <img src={image} key={idx} alt="project-image" className="project-image" />
+            </div>
+          );
+        })}
+      </CarouselContentsContainer>
+    </>
+  );
+};
+
+const CarouselContentsContainer = styled.div<CarouselBoxProps>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 95%;
+  padding: 0 20%;
+  background-color: gray;
+  transform: translateX(
+    ${({ imageNumber }) => {
+      return `${100 - imageNumber * 50}vw`;
+    }}
+  );
+  transition: ${({ carouselTransition }) => carouselTransition};
+
+  .image-box {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 50vw;
+    height: 95%;
+    .project-image {
+      min-width: 80%;
+      max-height: 100%;
+    }
+  }
+`;
+
+export default CarouselContents;
