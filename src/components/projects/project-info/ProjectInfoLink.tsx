750dc9790cb696510bae6c6c9c4d1df169f86922
kimsh322 <kimsh91322@gmail.com>
2023-09-23 16:49:43 +0900

    Fix : 프로젝트 링크 key prop 추가

--- src/components/projects/project-info/ProjectInfoLink.tsx
+++ src/components/projects/project-info/ProjectInfoLink.tsx
@@ -17,7 +17,7 @@ const ProjectInfoLink = ({ links }: Props) => {
       <div className="link-box">
         {Object.keys(links).map((link, idx) => {
           return (
-            <a href={links[link]} className="link" target="_blank">
+            <a href={links[link]} className="link" target="_blank" key={titleArr[idx]}>
               {titleArr[idx]}
             </a>
           );
